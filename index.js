5e520f7c22aebe9c286fc201467805e68bf0444f
Alex Hortin <hortinstein@gmail.com>
2013-03-24 22:07:18 -0400

    added basic file io to modify older config files

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-var prompts = require('./user_prompts/swarm_prompts.js');
+var read_config = function (path, callback){
+    var fs = require('fs');
+    try {
+        // Query the entry
+        var stats = fs.lstatSync(path);
+        // Is it a directory?
+        if (stats.isDirectory()) {
+            // Yes it is
+        }
+    }
+    catch (e) {
+        console.log('swarm configuration file not found, creating new'
+    }
+};
+
 //if the module is executed directly
 if (!module.parent) {
+    var prompts = require('./user_prompts/swarm_prompts.js');
+    var argv = require('optimist').argv;
 	prompts(function (e,r) {
 		console.log(r);
 	});
+} 
+//if the module is called from another program to modify existing json
+else {
+    
 }
